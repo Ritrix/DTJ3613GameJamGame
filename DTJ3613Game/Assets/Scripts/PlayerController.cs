52054c80889a3eb96880ded7e7ba7e2a57e20b52
Ritrix <ryanpauloneill@googlemail.com>
2025-04-26 14:13:05 +0100

    added movement and framework for light attack

--- DTJ3613Game/Assets/Scripts/PlayerController.cs
+++ DTJ3613Game/Assets/Scripts/PlayerController.cs
@@ -1,16 +1,63 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    [Header("Movement")]
+    public float runSpeed = 1.0f;
+    public InputAction moveAction;
+    Rigidbody2D playerRigidBody;
+    Vector2 move;
+
+    [Header("Attacks")]
+    public InputAction lightAttackAction;
+
+    bool isAttacking;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        //test
+        moveAction.Enable();
+        lightAttackAction.Enable();
+        playerRigidBody = GetComponent<Rigidbody2D>();
+        isAttacking = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // set move vector to current input
+        move = moveAction.ReadValue<Vector2>();
+
+        // stop moving when light attack pressed
+        if (lightAttackAction.IsPressed())
+        {
+            isAttacking = true;
+            if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+            {
+                move.x = 0.0f;
+                move.y = 0.0f;
+            }
+        }
     }
+
+    public void alertObservers(string message)
+    {
+        if (message == "LightAttackEnd")
+            isAttacking = false;
+    }
+
+    private void FixedUpdate()
+    {
+
+        
+        // move player character
+        if (!isAttacking)
+        {
+            Vector2 position = (Vector2)playerRigidBody.position + move * runSpeed * Time.deltaTime;
+            playerRigidBody.MovePosition(position);
+        }
+    }
+
+
 }
