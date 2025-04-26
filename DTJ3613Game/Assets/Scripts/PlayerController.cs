9f10f6844f4d66f694108e19d93f4fcd336d7b2d
Ritrix <ryanpauloneill@googlemail.com>
2025-04-26 23:10:20 +0100

    added player stun, fixed isAttacking glitch

--- DTJ3613Game/Assets/Scripts/PlayerController.cs
+++ DTJ3613Game/Assets/Scripts/PlayerController.cs
@@ -13,7 +13,7 @@ public class PlayerController : MonoBehaviour
     [Header("Attacks")]
     public InputAction lightAttackAction;
     [SerializeField] private Animator animator;
-    bool isAttacking;
+    public bool isAttacking;
     int typeAttack; // type of attack: Not attacking = 0, Light = 1, medium = 2, special = 3
     [SerializeField] private Transform playerVisual;
     private Vector3 originalPlayerScale; // keeping track of the original player scale as we flip him around
@@ -28,7 +28,9 @@ public class PlayerController : MonoBehaviour
     float damageCooldown;
 
 
-    bool blocking;
+    public bool blocking;
+    private bool stun;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -48,47 +50,12 @@ public class PlayerController : MonoBehaviour
     {
         // set move vector to current input
         move = moveAction.ReadValue<Vector2>();
-        // if moving set move animation to true
-        if(!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
-        {
-            animator.SetBool("isWalking", true);
-            blocking = false;
-        }
-        else
-        {
-            animator.SetBool("isWalking", false);
-            if (!isAttacking)
-            {
-                blocking = true;
-            }
-        }
 
-        // change player direction
-        if (!isAttacking)
-        {
-            blocking = false;
-            if (move.x > 0.0f)
-            {
-                playerVisual.transform.localScale = originalPlayerScale; // have player face right
-            }
-            else if (move.x < 0.0f)
-            {
-                playerVisual.transform.localScale = new Vector3(-originalPlayerScale.x, originalPlayerScale.y, originalPlayerScale.z);
-            }
-        }
+        // function to handle all movement related items and controls
+        movement();
 
-        // stop moving when light attack pressed
-        if (lightAttackAction.IsPressed() && typeAttack < 1)
-        {
-            animator.SetTrigger("isLAttackingSTrigger");
-            isAttacking = true;
-            typeAttack = 1;
-            if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
-            {
-                move.x = 0.0f;
-                move.y = 0.0f;
-            }
-        }
+        // function to handle all attack related items and controls
+        attacks();
 
         if (isInvincible)
         {
@@ -111,6 +78,12 @@ public class PlayerController : MonoBehaviour
             isAttacking = false;
             typeAttack = 0;
         }
+        else if(message == "stunEnd")
+        {
+            isAttacking = false;
+            typeAttack = 0;
+            stun = false;
+        }
             
 
     }
@@ -118,7 +91,7 @@ public class PlayerController : MonoBehaviour
     private void FixedUpdate()
     {
         // move player character
-        if (!isAttacking)
+        if (!isAttacking && !stun)
         {
             Vector2 position = (Vector2)playerRigidBody.position + move * runSpeed * Time.deltaTime;
             playerRigidBody.MovePosition(position);
@@ -133,6 +106,7 @@ public class PlayerController : MonoBehaviour
     {
         if (amount < 0)
         {
+            stun = true;
             if (isInvincible)
             {
                 return;
@@ -163,11 +137,73 @@ public class PlayerController : MonoBehaviour
     /// <param name="collision"></param>
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "basicEnemyAttackHitbox" && blocking == false)
+        if (collision.gameObject.tag == "basicEnemyAttackHitbox")
         {
-            changeHealth(-1);
-            animator.SetTrigger("playerHurt");
+            if (!blocking)
+            {
+                changeHealth(-1);
+                animator.SetTrigger("playerHurt");
+            }
+            else
+            {
+
+            }
         }
     }
+
+    #region private custom functions
+
+    private void movement()
+    {
+        // if moving set move animation to true
+        if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
+            animator.SetBool("isWalking", true);
+            blocking = false;
+        }
+        else
+        {
+            animator.SetBool("isWalking", false);
+            if (!isAttacking)
+            {
+                blocking = true;
+            }
+        }
+
+        // change player direction
+        if (!isAttacking)
+        {
+            if (move.x > 0.0f)
+            {
+                playerVisual.transform.localScale = originalPlayerScale; // have player face right
+            }
+            else if (move.x < 0.0f)
+            {
+                playerVisual.transform.localScale = new Vector3(-originalPlayerScale.x, originalPlayerScale.y, originalPlayerScale.z);
+            }
+        }
+    }
+
+    private void attacks()
+    {
+        if (!stun)
+        {
+            // stop moving when light attack pressed
+            if (lightAttackAction.IsPressed() && typeAttack < 1)
+            {
+                animator.SetTrigger("isLAttackingSTrigger");
+                isAttacking = true;
+                blocking = false;
+                typeAttack = 1;
+                if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+                {
+                    move.x = 0.0f;
+                    move.y = 0.0f;
+                }
+            }
+        }
+    }
+
+    #endregion
 
 }
