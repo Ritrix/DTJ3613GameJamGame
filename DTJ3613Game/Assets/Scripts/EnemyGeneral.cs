31c962fc68b8eff4f4569582f44d2ae239c1c833
Ritrix <ryanpauloneill@googlemail.com>
2025-04-26 18:31:56 +0100

    added attack hitbox

--- DTJ3613Game/Assets/Scripts/EnemyGeneral.cs
+++ DTJ3613Game/Assets/Scripts/EnemyGeneral.cs
@@ -2,15 +2,17 @@ using UnityEngine;
 
 public class EnemyGeneral : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField] private Animator m_Animator;
+
+    private const string HIT_PARAM = "isHit";
+    private const string PLAYER_LIGHT1_HITBOX = "playerHitboxLight1";
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if(collision.gameObject.tag == PLAYER_LIGHT1_HITBOX)
+        {
+            m_Animator.SetTrigger(HIT_PARAM);
+        }
     }
 }
